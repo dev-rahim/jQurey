2f6bcc20dee515d10027c9061775244bb7a16ddf
dev-rahim <rahimuddin5915@gmail.com>
2022-06-15 00:44:03 +0600

    jQurey show hide togle

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,28 @@
-// EXTARNAL JS 
-$(document).ready(function() {
-    //CODE
-});
+// // EXTARNAL JS 
+// $(document).ready(function() {
+//     //CODE
+// });
 
-$(window).load(function(){
-    //CODE
-})
+// $(window).load(function(){
+//     //CODE
+// })
+
+// $(document).ready(function() {
+//     $('h2').click(function(){
+//         $('p').hide();
+//     })
+
+//    });
+
+//    $(document).ready(function() {
+//     $('h2').click(function(){
+//         $('p').show();
+//     })
+
+//    });
+   $(document).ready(function() {
+    $('h2').click(function(){
+        $('p').toggle();
+    })
+
+   });
