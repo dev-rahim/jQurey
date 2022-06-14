bc9fe8a613a60eb29092e403fdd80f3432d632e6
dev-rahim <rahimuddin5915@gmail.com>
2022-06-15 02:00:40 +0600

    some more events

--- js/custom.js
+++ js/custom.js
@@ -52,11 +52,32 @@
     $('h2').click(function(){
         $('p').html("<h4>Ki Khobor?</h4>");
         $('p').css('color', '#900000');
+        //add class
+        // $('h4').addClass('addedBy_jquery');
+        // empty 
+        // $('h4').empty();
+        // reove 
+        // $('h4').remove();
+        // append 
+        $('h4').append('123');
+        // prepand
+        $('h4').prepend('p2253');
+        //after
+        $('h4').after('after');
+        //before
+        $('h4').before('before');
+        // attr 
+        $('h4').attr('class', 'bg-primary');
+        
 
     })
 
     $('h3').click(function(){
         $('h3').text('Request Sent');
+        // remove class 
+        // $('h3').removeClass('btn-primary');
+        // toogle class 
+        $('h3').toggleClass('btn-primary');
     })
 
    });
