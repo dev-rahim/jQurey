7eae4c2b11cf743125561f905af1509beeee75ea
dev-rahim <rahimuddin5915@gmail.com>
2022-06-15 01:27:21 +0600

    jQurey text html css event

--- js/custom.js
+++ js/custom.js
@@ -20,9 +20,43 @@
 //     })
 
 //    });
+//    $(document).ready(function() {
+//     $('h2').click(function(){
+//         $('p').toggle();
+//     })
+
+//    });
+//    $(document).ready(function() {
+//     $('h2').click(function(){
+//         $('p').slideUp(4000);
+//     })
+
+//    });
+//    $(document).ready(function() {
+//     $('h2').click(function(){
+//         $('p').slideToggle();
+//     })
+
+//    });
+//    $(document).ready(function() {
+//     $('h2').click(function(){
+//         $('p').fadeToggle();
+//     })
+
+//    });
    $(document).ready(function() {
+    // $('h2').click(function(){
+    //     $('p').fadeTo('slow', 0.1);
+    // $('h2').click(function(){
+    //     $('p').text('Ki khobor?');
     $('h2').click(function(){
-        $('p').toggle();
+        $('p').html("<h4>Ki Khobor?</h4>");
+        $('p').css('color', '#900000');
+
+    })
+
+    $('h3').click(function(){
+        $('h3').text('Request Sent');
     })
 
    });
